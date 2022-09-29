488dbe6253e9b81b170176f74897b32d16d52969
laura tascon <laura.tascon@udea.edu.co>
2022-09-28 22:52:48 -0500

    cambia settings

--- gestion_paquetes/settings.py
+++ gestion_paquetes/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'paquetes',
         'USER': 'postgres',
-        'PASSWORD': 'Tascon1234W',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
